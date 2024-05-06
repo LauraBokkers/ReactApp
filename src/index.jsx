5ad04d5fde60f4c84db33a08e2e71900346f5f4f
LauraAnneB <88866166+LauraAnneB@users.noreply.github.com>
2024-05-06 16:56:22 +0200

    Split up components to make App more lean

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+//import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
@@ -5,6 +6,7 @@ import "./index.css";
 
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App />);
+//ReactDOM.createRoot(entryPoint).render(React.createElement(App));
 
 
 
